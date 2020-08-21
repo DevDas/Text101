9ae21b49c5995e785b4dee1e5e8dba4c4a2c8b10
DevDas <55701713+DevDas@users.noreply.github.com>
2020-08-21 16:16:54 +0530

    NumberWizard UI Complete

--- Text101/Assets/NumberWizardUI/Scripts/SceneLoader.cs
+++ Text101/Assets/NumberWizardUI/Scripts/SceneLoader.cs
@@ -2,12 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI TextComponent;
+
+    [SerializeField] int Min;
+    [SerializeField] int Max;
+    int Guess;
+
     public void LoadNextScene()
     {
         int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(CurrentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void SetHigher()
+    {
+        if (Guess < Max)
+        {
+            Min = Guess + 1;
+        }
+        CalculateGuess();
+    }
+
+    public void SetLower()
+    {
+        if (Guess > Min)
+        {
+            Max = Guess - 1;
+        }
+        CalculateGuess();
+    }
+
+    private void CalculateGuess()
+    {
+        //int OurGuess = (Min + Max) / 2;
+
+        int OurGuess = Random.Range(Min, Max + 1);
+        Guess = OurGuess;
+        TextComponent.text = Guess.ToString();
+    }
+
+    void Start()
+    {
+        CalculateGuess();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
